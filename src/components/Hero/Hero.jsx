f95f2c12a91b34759e18abb276decaebee1c98c3
Sanskar-Sakhareliya <sanskar.sakhareliya@bjtmail.com>
2024-03-25 16:12:37 +0530

    Testing

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,6 +4,40 @@ import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
 import { motion } from "framer-motion";
 import Typewriter from 'typewriter-effect';
+import 'react-slideshow-image/dist/styles.css'
+
+import { Slide } from 'react-slideshow-image';
+import { Fade } from 'react-slideshow-image';
+
+const spanStyle = {
+  padding: '20px',
+  background: '#efefef',
+  color: '#000000'
+}
+
+const divStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundSize: 'cover',
+  height: '550px'
+}
+const slideImages = [
+  {
+    url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    caption: 'Slide 1'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+    caption: 'Slide 2'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    caption: 'Slide 3'
+  },
+];
+
+
 
 const Hero = () => {
   const [sentence, setSentence] = useState(
@@ -90,7 +124,18 @@ const Hero = () => {
             }}
             className="image-container"
           >
-            <img src="https://github.com/Sanskar0124/Fabrics-Landing-Page/blob/master/assets/gifs/WhatsAppVideo2024-03-16at18.03.51_5141d442-ezgif.com-video-to-gif-converter.gif?raw=true" alt="houses" />
+            <div className="slide-container">
+              <Slide>
+                {slideImages.map((slideImage, index) => (
+                  <div key={index}>
+                    <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+                      {/* <span style={spanStyle}>{slideImage.caption}</span> */}
+                    </div>
+                  </div>
+                ))}
+              </Slide>
+            </div>
+            {/* <img src="https://github.com/Sanskar0124/Fabrics-Landing-Page/blob/master/assets/gifs/WhatsAppVideo2024-03-16at18.03.51_5141d442-ezgif.com-video-to-gif-converter.gif?raw=true" alt="houses" /> */}
           </motion.div>
         </div>
       </div>
